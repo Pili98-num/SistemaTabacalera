ae64002bfab04a6fd5f1071b97343451dde197af
Pili98-num <rpnunez@utpl.edu.ec>
2024-12-17 23:39:58 -0500

    asd

--- pages/solicitude/edit/[id].tsx
+++ pages/solicitude/edit/[id].tsx
@@ -356,9 +356,9 @@ export const EditSolicitude = () => {
                           >
                             <path
                               stroke="currentColor"
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                              stroke-width="2"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="2"
                               d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
                             />
                           </svg>
@@ -395,7 +395,7 @@ export const EditSolicitude = () => {
                           )}
                         </tr>
                       </thead>
-                      <tbody>
+                      {/* <tbody>
                         <>
                           {sortItemsByName()}
                           {(filterItems() ?? []).map((item, index) => {
@@ -457,7 +457,7 @@ export const EditSolicitude = () => {
                             );
                           })}
                         </>
-                      </tbody>
+                      </tbody> */}
                     </table>
                   </div>
                 </>
